14b269aef161361320233747d255f442a0dd964d
Eric Freyer <ericcfreyer@gmail.com>
2020-12-07 16:24:13 -0500

    got hours to log

--- Develop/luxon.js
+++ Develop/luxon.js
@@ -3,10 +3,31 @@ $(document).ready(function () {
 
 var DateTime = luxon.DateTime;
 var date = (DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit', hour: '2-digit', minute: '2-digit'}));
-var dateHour = (DateTime.local().toLocaleString({hour: '2-digit'}))
-console.log(dateHour)
+//this changes the hour of DateTime
+
+//var dateHourFive = (DateTime.fromObject({hour: 5})) 
+//$("#currentDay").append(date)
+//$('#5pm').append(dateHourFive)
+var hourFive = (DateTime.fromObject({hour: 17}))
+
+var now = DateTime.local();
+
+//having trouble calling the hour from DateTime
 console.log(date)
-$("#currentDay").append(date)
+console.log(hourFive.hour)
+console.log(now.hour)
+
+
+
+
+
+
+//if ((dateHourFive.hour)>(date.hour)){
+
+//}
+
+
+//use dateHour interval for each row??
 
 
 //hourOfRow = ???? how to make hour of row = to certain number
@@ -15,17 +36,17 @@ $("#currentDay").append(date)
 //var currentHour = hourOfRow
 
 
-//if dateHour===currentHour{
-    //addclass(".present")
+//if (dateHour===currentHour){
+  //  addclass(".present")
 //}
 
 
-//if dateHour>currentHour{
+//if (dateHour>currentHour){
     //addclass(".past")
 //}
 
-//if dateHour<currentHour{
-    //addclass(".future")
+//if (date>dateHourFive){
+  //  dateHourFive.addclass(".future")
 //}
 
 
